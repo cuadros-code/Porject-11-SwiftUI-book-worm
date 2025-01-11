bd508e7c6b61ce82674c10695410f50803f5f5f8
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-11 16:41:08 -0500

    Initial Commit

---
+++ Porject-11-SwiftUI-book-worm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Porject-11-SwiftUI-book-worm
+//
+//  Created by Kevin Cuadros on 11/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Porject-11-SwiftUI-book-worm/Porject_11_SwiftUI_book_wormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Porject_11_SwiftUI_book_wormApp.swift
+//  Porject-11-SwiftUI-book-worm
+//
+//  Created by Kevin Cuadros on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Porject_11_SwiftUI_book_wormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
